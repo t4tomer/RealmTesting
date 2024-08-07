d31b771fae8412e6fac5b584819d3ce014c837e2
t4tomer <tomer.polsky27@gmail.com>
2024-08-07 10:56:30 +0300

    added the original  RealmToDo example
    added this original example of the RealmToDo example .

---
+++ obj/Debug/net7.0-android/android/src/crc6445a4ae5df7ce5659/MainActivity.java
@@ -0,0 +1,48 @@
+package crc6445a4ae5df7ce5659;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("RealmTodo.MainActivity, RealmTodo", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("RealmTodo.MainActivity, RealmTodo", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("RealmTodo.MainActivity, RealmTodo", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
