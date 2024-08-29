a1efb95667e36af5d97d64bad3c4610f6da3b561
t4tomer <tomer.polsky27@gmail.com>
2024-08-29 12:43:39 +0300

    update

--- obj/Debug/net7.0-windows10.0.19041.0/win10-x64/RealmTodo.AssemblyInfo.cs
+++ obj/Debug/net7.0-windows10.0.19041.0/win10-x64/RealmTodo.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealmTodo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b5994f006a723750d9d182bc8bc9681845f6d47a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+759ad4131acf6581bb85211c4fac812e6006a926")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealmTodo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealmTodo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
