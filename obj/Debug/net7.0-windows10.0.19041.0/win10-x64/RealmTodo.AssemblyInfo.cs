bb5def3795158827dfa179bd90a4316ecb49c976
t4tomer <tomer.polsky27@gmail.com>
2024-12-09 18:07:08 +0200

    added file MapsViewModel

--- obj/Debug/net7.0-windows10.0.19041.0/win10-x64/RealmTodo.AssemblyInfo.cs
+++ obj/Debug/net7.0-windows10.0.19041.0/win10-x64/RealmTodo.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealmTodo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a6676d2cb377bce408e7c01ed1de5c2caf265e42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a43e83d4a9c71099d89f9833aa27a5c75d18a6c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealmTodo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealmTodo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
